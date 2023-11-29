ecdf653940119bf76787779f4a1ca1641aa12fd1
rohithim <151112090+rohithim@users.noreply.github.com>
2023-11-29 16:26:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./styles.css";
+import About from "./About";
+import Shop from "./Shop";
+import Nav from "./Nav";
+import WeatherApp from "./WeatherApp";
+import LoginSignup from "./Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+export default function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" exact element={<WeatherApp />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/WeatherApp" element={<WeatherApp/>}/>
+          <Route path="/LoginSignup" element={<LoginSignup/>}/>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h1>Home page</h1>;
+}
